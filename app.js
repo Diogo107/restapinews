8c5b555222dd3c2823bb03b40c65f518692c21aa
Diogo Santos <diogo.filipe.santos107@gmail.com>
2021-01-13 16:42:58 +0000

    Update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
-const port = process.env.NODE_ENV || 5000;
+const port = process.env.PORT || 5000;
 //console.log('===> '. process.env.NODE_ENV)
 
 app.get('/ping', (req, res) => {
@@ -15,5 +15,5 @@ app.get('*', (req, res) => {
     res.send('Try to go to /ping');
 });
 
-app.listen(port);
+app.listen(3000);
 console.log(`Listening on port: ${port}`);
