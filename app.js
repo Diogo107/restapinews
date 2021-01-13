95b952460707a9fc979799dab27403d2a227e8b8
Diogo Santos <diogo.filipe.santos107@gmail.com>
2021-01-13 20:36:59 +0000

    Test on DB

--- app.js
+++ app.js
@@ -3,10 +3,18 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const TopNews = require('./Schemas/topNews')
 const PORT = process.env.PORT || 3000;
 
 
-app.get('/ping', (req, res) => {
+app.get('/ping', async (req, res) => {
+    TopNews.create( {
+    title: "String",
+    subtitle: "String",
+    imageUrl: "String",
+    content: ["String", "number"],
+    url: "asdasdasd"+Math.floor(100*Math.random())
+  })
     res.send('===> pong');
 });
 
