ee3c37b15d102430312bcd6e59b6aac85a9e1367
Diogo Santos <diogo.filipe.santos107@gmail.com>
2021-01-13 16:04:48 +0000

    Test

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+'use strict';
+
+require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = 3000;
+
+const port = process.env.NODE_ENV || 3000;
+
+console.log('================> ', port)
+//console.log('===> '. process.env.NODE_ENV)
 
 app.get('/ping', (req, res) => {
     res.send('===> pong');
